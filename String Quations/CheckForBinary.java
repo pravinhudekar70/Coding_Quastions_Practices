c5f466e44f328da4e9df98c56fad87cb5bc679e5
pravinhudekar70 <pravinhudekar70@gmail.com>
2024-11-22 10:27:30 +0530

    string program created

---
+++ String Quations/CheckForBinary.java
@@ -0,0 +1,13 @@
+/*Input: str = "01010101010"
+Output: Yes
+
+Input: str = "geeks101"
+Output: No
+*/
+
+
+public class CheckForBinary {
+    public static void main(String[] args) {
+        
+    }
+}
